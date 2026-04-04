c22f7223d9e08f0d6ca4bb151168b87d1deaa94c
Cedric <cedricctf11a@gmail.com>
2026-04-04 13:24:50 +0200

    changed nav to minikit page

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,7 @@ export default function Home() {
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <Link
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="/world"
+            href="/minikit"
           >
             World ID Demo
           </Link>
