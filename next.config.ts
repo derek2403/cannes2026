0a49248b5f3ce336b8d29883b0d59a54efa8aa51
Cedric <cedricctf11a@gmail.com>
2026-04-04 02:30:14 +0200

    support minikit and idkit server external package

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  serverExternalPackages: ["@worldcoin/minikit-js"],
+  serverExternalPackages: ["@worldcoin/minikit-js", "@worldcoin/idkit-core"],
 };
 
 export default nextConfig;
