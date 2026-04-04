5fd1201220f3f7f0613cf9972f86c6d936c83c52
Cedric <cedricctf11a@gmail.com>
2026-04-04 02:23:05 +0200

    wrapped app with minikit provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { MiniKitProvider } from "@worldcoin/minikit-js/minikit-provider";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MiniKitProvider>
+      <Component {...pageProps} />
+    </MiniKitProvider>
+  );
 }
