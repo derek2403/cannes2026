4ced1aedc2b59d5f8fcec6a93d111c6c34ff40d7
Cedric <cedricctf11a@gmail.com>
2026-04-04 02:22:40 +0200

    config nextjs for minikit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  serverExternalPackages: ["@worldcoin/minikit-js"],
 };
 
 export default nextConfig;
