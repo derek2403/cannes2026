17b75bbb0885f3484f9cc98fa7971c426a36a20c
Cedric <cedricctf11a@gmail.com>
2026-04-04 12:52:23 +0200

    add sepolia in hardhat config

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import hardhatEthersPlugin from "@nomicfoundation/hardhat-ethers";
 import hardhatIgnitionEthersPlugin from "@nomicfoundation/hardhat-ignition-ethers";
-import { defineConfig } from "hardhat/config";
+import { defineConfig, configVariable } from "hardhat/config";
 
 export default defineConfig({
   plugins: [hardhatEthersPlugin, hardhatIgnitionEthersPlugin],
@@ -31,8 +31,10 @@ export default defineConfig({
     zgTestnet: {
       type: "http",
       chainType: "l1",
-      url: process.env.ZG_TESTNET_RPC_URL!,
-      accounts: [process.env.ZG_TESTNET_PRIVATE_KEY!],
+      url: process.env.ZG_TESTNET_RPC_URL || "http://localhost:8545",
+      accounts: process.env.ZG_TESTNET_PRIVATE_KEY
+        ? [process.env.ZG_TESTNET_PRIVATE_KEY]
+        : [],
     },
     worldChainSepolia: {
       type: "http",
@@ -40,6 +42,13 @@ export default defineConfig({
       chainId: 4801,
       url: configVariable("WORLD_CHAIN_SEPOLIA_RPC_URL"),
       accounts: [configVariable("WORLD_CHAIN_SEPOLIA_PRIVATE_KEY")],
+    },
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      chainId: 11155111,
+      url: configVariable("SEPOLIA_RPC_URL"),
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
   },
 });
