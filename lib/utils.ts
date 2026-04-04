914f2feb9737a779d44d5be44c62a6087c5f3f4a
derek2403 <derekliew0@gmail.com>
2026-04-04 16:42:45 +0200

    refactor: replace custom cn utility with clsx and tailwind-merge and reorganize package dependencies

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,3 @@
-export function cn(...classes: (string | undefined | false | null)[]) {
-  return classes.filter(Boolean).join(" ");
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
